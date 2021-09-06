b738f8bc1f259bdf44f0aa5b3bda84707b87ab0c
shubhamgosavi95 <86293329+shubhamgosavi95@users.noreply.github.com>
2021-09-06 11:34:33 +0530

    added a comment line

--- boards/apps.py
+++ boards/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# added new line from git 
 class BoardsConfig(AppConfig):
     """This is boards configuretion"""
     default_auto_field = 'django.db.models.BigAutoField'
