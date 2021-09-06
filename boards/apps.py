b345f855ffe9a9ef6c2ef9a0eb91a545549d18ab
shubhamgosavi95 <shubhamgosavi95@gmail.com>
2021-09-06 11:32:44 +0530

    Added docstring in app

--- boards/apps.py
+++ boards/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class BoardsConfig(AppConfig):
+    """This is boards configuretion"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'boards'
